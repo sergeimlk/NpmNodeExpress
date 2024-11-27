48f6b07b9cb43059e4a154073f5cb986784e1ff7
sergeimlk <sergei.mlk@hotmail.com>
2024-11-27 10:05:23 +0100

    GET Ok! "message": "role \"zythologue_user\" does not exist"

--- src/routes/beers.ts
+++ src/routes/beers.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 export const router = Router();
 
-import { getBeersController } from "../controllers/beersController";
+import { beersController } from "../controllers/beersController";
 // import { postBeersController } from "../controllers/beersController";
 // import { putBeersController } from "../controllers/beersController";
 // import { deleteBeersController } from "../controllers/beersController";
 // import { getDetailsBeersController } from "../controllers/beersController";
 
-router.get('/', getBeersController.get);
+router.get('/', beersController.get);
 // router.post('/', postBeersController.post);
 // router.put('/', putBeersController.put);
 // router.delete('/:id', deleteBeersController.delete);
